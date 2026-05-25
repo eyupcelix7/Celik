27087edb28473e0738731d9a82289cdefa907f68
eyupcelix7 <eyupturkey641@gmail.com>
2026-05-25 23:21:45 +0300

    Add project files.

---
+++ Modul.7_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+
+ArrayList sayilar = new();
+
+int[] num = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+for (int i = 0; i < 10; i++)
+{
+    sayilar.Add(num[i]);
+}
+sayilar.RemoveAt(2);
+
+sayilar.Insert(5, 2345);
+
+for (int i = 0; i < sayilar.Count; i++)
+{
+    Console.WriteLine(sayilar[i]);
+}
+Console.ReadKey();
